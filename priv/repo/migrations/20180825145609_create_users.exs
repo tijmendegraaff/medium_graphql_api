e2b5207ca7c29c9eddb3897bf55550f608bee24e
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-27 00:46:44 +0200

    add unique index to user email field in migration

--- priv/repo/migrations/20180825145609_create_users.exs
+++ priv/repo/migrations/20180825145609_create_users.exs
@@ -3,14 +3,15 @@ defmodule MediumGraphqlApi.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :first_name, :string
-      add :last_name, :string
-      add :email, :string
-      add :password_hash, :string
-      add :role, :string
+      add(:first_name, :string)
+      add(:last_name, :string)
+      add(:email, :string)
+      add(:password_hash, :string)
+      add(:role, :string)
 
       timestamps()
     end
 
+    create(unique_index(:users, [:email]))
   end
 end
