4503807921a812abcb704d6847d979bd8ec1576b
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-27 00:46:24 +0200

    implement user model validation

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -3,11 +3,13 @@ defmodule MediumGraphqlApi.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field(:email, :string)
+    field(:email, :string, unique: true)
     field(:first_name, :string)
     field(:last_name, :string)
     field(:password_hash, :string)
-    field(:role, :string)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
 
     timestamps()
   end
@@ -15,7 +17,24 @@ defmodule MediumGraphqlApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+    |> cast(attrs, [:first_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_required([
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    ])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
