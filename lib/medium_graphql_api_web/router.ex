5fff4a3295081897bc7f1d09a9be871399ca7c17
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-31 01:58:07 +0200

    add graphql endpoints

--- lib/medium_graphql_api_web/router.ex
+++ lib/medium_graphql_api_web/router.ex
@@ -5,7 +5,13 @@ defmodule MediumGraphqlApiWeb.Router do
     plug(:accepts, ["json"])
   end
 
-  scope "/api", MediumGraphqlApiWeb do
+  scope "/api" do
     pipe_through(:api)
+
+    forward("/graphql", Absinthe.Plug, schema: MediumGraphqlApiWeb.Schema)
+
+    if Mix.env() == :dev do
+      forward("/graphiql", Absinthe.Plug.GraphiQL, schema: MediumGraphqlApiWeb.Schema)
+    end
   end
 end
