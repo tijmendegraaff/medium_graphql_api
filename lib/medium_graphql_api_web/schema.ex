df6a9ca4d2e074aaad0ad2f700f3aa03582f3712
Tijmen de Graaff <tijmen76@gmail.com>
2018-09-14 15:41:27 +0200

    add Login Mutation to schema

--- lib/medium_graphql_api_web/schema.ex
+++ lib/medium_graphql_api_web/schema.ex
@@ -19,6 +19,12 @@ defmodule MediumGraphqlApiWeb.Schema do
       arg(:input, non_null(:user_input_type))
       resolve(&Resolvers.UserResolver.register_user/3)
     end
+
+    @desc "Login a user and return a JWT token"
+    field :login_user, type: :session_type do
+      arg(:input, non_null(:session_input_type))
+      resolve(&Resolvers.SessionResolver.login_user/3)
+    end
   end
 
   # subscription do
