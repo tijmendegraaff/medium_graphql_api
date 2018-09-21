bfa9303abb149277fca161543df4fa9310430faf
Tijmen de Graaff <tijmen76@gmail.com>
2018-09-21 22:04:02 +0200

    add comment and post mutation to schema file

--- lib/medium_graphql_api_web/schema.ex
+++ lib/medium_graphql_api_web/schema.ex
@@ -27,6 +27,20 @@ defmodule MediumGraphqlApiWeb.Schema do
       arg(:input, non_null(:session_input_type))
       resolve(&Resolvers.SessionResolver.login_user/3)
     end
+
+    @desc "Create a post"
+    field :create_post, type: :post_type do
+      arg(:input, non_null(:post_input_type))
+      middleware(Middleware.Authorize, :any)
+      resolve(&Resolvers.PostResolver.create_post/3)
+    end
+
+    @desc "Create a comment"
+    field :create_comment, type: :comment_type do
+      arg(:input, non_null(:comment_input_type))
+      middleware(Middleware.Authorize, :any)
+      resolve(&Resolvers.CommentResolver.create_comment/3)
+    end
   end
 
   # subscription do
